9d6be7567d6b250a3f6ce93e883a54fa29c442c8
karolek2155 <paz.karol.glowne@gmail.com>
2023-11-15 09:37:20 +0100

    kod

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+// 1 str z klawiatury, przenies 2 ostatnie znaki na przod, pod warunkiem, ze tych znakow jest na tyle co najmniej 3, jesli nie - zostawisz bez
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków o długości co najmniej 3 :");
+        String str = scanner.nextLine();
+
+        int lengthStr = str.length();
+        if (str.length() > 2) {
+            String last2Chars = str.substring(lengthStr-2, lengthStr);
+            String chars = str.substring(0, lengthStr-2);
+            String newStr = last2Chars + chars;
+            System.out.println(newStr);
+        }
+        else{
+            System.out.println("Podaj ciąg znaków o długości co najmniej 3");
+        }
 
     }
 }
