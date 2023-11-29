916881dfcbb8bbca4ca1c13972c1f8ebb0182eac
karolek2155 <paz.karol.glowne@gmail.com>
2023-11-29 09:00:14 +0100

    Klasa Math

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import static java.lang.Math.*;
 public class Main {
     public static void main(String[] args) {
 
 // 1 str z klawiatury, przenies 2 ostatnie znaki na przod, pod warunkiem, ze tych znakow jest na tyle co najmniej 3, jesli nie - zostawisz bez
 
-        Scanner scanner = new Scanner(System.in);
+/*        Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj ciąg znaków o długości co najmniej 3 :");
         String str = scanner.nextLine();
 
@@ -18,6 +19,60 @@ public class Main {
         else{
             System.out.println("Podaj ciąg znaków o długości co najmniej 3");
         }
+
+        System.out.println("Podaj ciąg znaków :");
+        String str2 = scanner.nextLine();
+        System.out.println("Podaj dowolny indeks ciągu znaków :");
+        int indexStr2 = scanner.nextInt();
+
+        if (indexStr2 + 4 <= str2.length()) {
+            String newStr2 = str2.substring(indexStr2, indexStr2+4);
+            System.out.println(newStr2);
+        }
+       */
+
+//        double result = Math.pow(5,2);
+//        System.out.println(result);
+
+        /*Scanner scann = new Scanner(System.in);
+        System.out.println("Podaj kolejno a, b, c trójmianu kwadratowego");
+        double a = scann.nextDouble();
+        double b = scann.nextDouble();
+        double c = scann.nextDouble();
+
+        double delta = pow(b, 2) - 4 * a * c;
+
+        if (a == 0) {
+            double x = -c / b;
+            System.out.printf("Równanie liniowe - rozw. to %.2f\n", x);
+        }else {
+            if (delta > 0) {
+                double x1 = (-b - sqrt(delta) / (2 * a));
+                double x2 = (-b + sqrt(delta) / (2 * a));
+                System.out.printf("Pierwiastkami równania kwadratowego są: %.2f i %.2f\n", x1, x2);
+            } else if (delta == 0) {
+                double x0 = -b / (2 * a);
+                System.out.printf("Równanie kwadratowe ma jeden pierwiastek %.2f\n", x0);
+            } else System.out.println("Równanie nie ma pierwiastków");
+        }
+*/
+
+/*        double[] tabDouble = new double[20];
+
+        for (int i = 0; i < tabDouble.length; i++){
+            tabDouble[i] = random() * 40 + 30;
+            System.out.print(tabDouble[i] + " ");
+        }
+
+        System.out.println("<br>");
+
+        int[] tabInt = new int[20];
+
+        for (int i = 0; i < tabInt.length; i++){
+            tabInt[i] = (int) (random() * 40 + 30);
+            System.out.print(tabInt[i] + " ");
+        }
+        */
 
     }
 }
